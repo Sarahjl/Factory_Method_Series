ee3aadb24232206ffe5aaea5f48802a82518739a
Sarahjl <sjandoza18@gmail.com>
2025-09-25 21:10:04 -0300

    revertido para o commit anterior

--- src/padrao/Principal.java
+++ src/padrao/Principal.java
@@ -1,11 +1,32 @@
 package padrao;
 
+import factory.ComedyDialog;
 import factory.SciFiDialog;
 import factory.Dialog;
+import java.util.Scanner;
 
 public class Principal {
+    private static Dialog dialog;
+    
     public static void main(String[] args) {
-        Dialog dialog = new SciFiDialog();
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Escolha um gênero de série (comedia ou scifi): ");
+        String escolha = entrada.nextLine().toLowerCase();
+        
+        switch(escolha){
+            case "comedia":
+                dialog = new ComedyDialog();
+                break;
+            case "scifi":
+                dialog = new SciFiDialog();
+                break;
+            default:
+                System.out.println("Não encontrado");
+                return;
+        }
+
+        System.out.println("");
         dialog.recomendar();
     }
 }
